b29689b462ba523dcf2b8d78d9b1a364fc68257c
Daniel Guzman <daniel_guzman.87@hotmail.com>
2022-03-30 18:05:30 -0600

    migracion de base de datos

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_30_232209) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_30_235404) do
   create_table "categoria", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "productos", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
